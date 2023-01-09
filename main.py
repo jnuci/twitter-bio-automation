6e7f672ad2d6e093a0ff8e776f836a77f4a62863
Jaime Nuci <jnuci@ucsb.edu>
2023-01-09 14:45:27 -0800

    Delete .env file

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import tweepy
 from dotenv import load_dotenv
+import os
+load_dotenv()
 
-api_key = config.API_key
-api_secret = config.API_secret
-user_key = config.Access_token
-user_secret = config.Access_secret
+api_key = os.getenv("API_key")
+api_secret = os.getenv("API_secret")
+user_key = os.getenv("Access_token")
+user_secret = os.getenv("Access_secret")
 
 # authorization of consumer key and consumer secret
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-client = tweepy.Client(bearer_token=config.Bearer_token)
+auth = tweepy.OAuthHandler(api_key, api_secret)
 
 # set access to user's access key and access secret 
-auth.set_access_token(access_token, access_token_secret)
+auth.set_access_token(user_key, user_secret)
   
 # calling the api 
 api = tweepy.API(auth)
   
 # the profile name to be updated
-name = "api test"
+text = "api test"
 
-user_profile = api.update_profile(name)
+api.update_profile(description = text)
